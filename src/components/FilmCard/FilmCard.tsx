1ce9ce3afcb9265bcd261062b62a79a405752c86
Daniil Dergunov <100157798+Rogepta@users.noreply.github.com>
2025-06-25 18:25:49 +0300

    Add skeleton for films

--- src/components/FilmCard/FilmCard.tsx
+++ src/components/FilmCard/FilmCard.tsx
@@ -3,19 +3,16 @@ import IconTransparentLike from '../icons/IconTransparentLike';
 import IconIFilledLike from '../icons/IconIFilledLike';
 import { useState } from 'react';
 import IconCross from '@components/icons/IconCross';
+import type { IFilm } from 'features/favorites/favoriteSlice';
 
 interface IFilmCardProps {
-  imageUrl: string;
-  title: string;
-  duration: string;
+  film: IFilm;
   isFavorite: boolean;
   onFavoriteToggle: (title: string) => void;
 }
 
 const FilmCard: React.FC<IFilmCardProps> = ({
-  imageUrl,
-  title,
-  duration,
+  film,
   isFavorite,
   onFavoriteToggle,
 }) => {
@@ -23,11 +20,15 @@ const FilmCard: React.FC<IFilmCardProps> = ({
 
   return (
     <div className={styles.container}>
-      <img className={styles.film_img} src={imageUrl} alt='момент из фильма' />
+      <img
+        className={styles.film_img}
+        src={film.imageUrl}
+        alt='момент из фильма'
+      />
       <div className={styles.film_title_bookmark}>
-        <p className={styles.film_title}>{title}</p>
+        <p className={styles.film_title}>{film.title}</p>
         <button
-          onClick={() => onFavoriteToggle(title)}
+          onClick={() => onFavoriteToggle(film.title)}
           onMouseEnter={() => isFavorite && setIsHovered(true)}
           onMouseLeave={() => isFavorite && setIsHovered(false)}
           style={{ position: 'relative' }}
@@ -41,7 +42,7 @@ const FilmCard: React.FC<IFilmCardProps> = ({
         </button>
       </div>
       <div className={styles.divider} />
-      <div className={styles.film_duration}>{duration}</div>
+      <div className={styles.film_duration}>{film.duration}</div>
     </div>
   );
 };
